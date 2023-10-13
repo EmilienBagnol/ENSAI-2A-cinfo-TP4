987695e14e75590647900c276a54accb87061fd4
Bastien Delmare <bastien.delmare2@gmail.com>
2023-10-13 14:30:41 +0200

    zd

---
+++ Carburants.py
@@ -0,0 +1,15 @@
+class Carburants:
+    def __init__(self, id_carburant, nom_carburant):
+        self.id_carburant = id_carburant
+        self.nom_carburant = nom_carburant
+
+    def __str__(self):
+        return f"Carburant {self.id_carburant}: {self.nom_carburant}"
+
+
+# Exemple d'utilisation de la classe
+carburant1 = Carburants(1, "Essence")
+carburant2 = Carburants(2, "Diesel")
+
+print(carburant1)  # Affiche : Carburant 1: Essence
+print(carburant2)  # Affiche : Carburant 2: Diesel
