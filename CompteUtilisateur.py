e792884a95f72553461a1173962938528407763c
emilien <emilien.bagnol@eleve.ensai.fr>
2023-10-13 16:17:07 +0200

    DAUUGFOU

---
+++ CompteUtilisateur.py
@@ -0,0 +1,17 @@
+class CompteUtilisateur:
+    def __init__(self,id_compte,mot_de_passe,identifiant):
+        self.id_compte = id_compte
+        self.mot_de_passe = mot_de_passe
+        self.identifiant = identifiant
+    
+    def créer_compte(self):
+        identifiant = input("Entrez votre identifiant :")
+        raise ValueError(identifiant is not string)
+    
+    def modifier_mdp(self):
+
+    def modifier_id(self):
+
+def créer_compte():
+    identifiant = input("Entrez votre identifiant :")
+    raise ValueError(identifiant is not string)
